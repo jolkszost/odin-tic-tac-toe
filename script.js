549ad9cd5606fd1afd4fb8f0e029a5a5b368804b
Jorian Olk-Szost <jolkszost@gmail.com>
2022-05-27 14:35:29 -0400

    add css, html, script for board, basic choice

---
+++ script.js
@@ -0,0 +1,71 @@
+
+
+
+
+const gameBoardModule = (function() {
+
+    let gameBoardArr = [];
+    
+    let gameBoard = document.getElementById('gameBoard');
+    function generateBoard() {
+        for (let i = 0; i < 9; i++) {
+           let sq = document.createElement('div');
+            gameBoard.appendChild(sq);
+            sq.classList.add('square');
+            sq.classList.add('empty');
+            sq.setAttribute('id', `${i}`);
+            sq.setAttribute('onclick', 'displayController.pickSq(this.id); displayController.clickCounter();');
+            // sq.setAttribute('onClick', 'displayController.clickCounter()')
+            
+           gameBoardArr.push(sq);
+            console.log(i);
+        }
+console.log(gameBoardArr)
+    } 
+    return {
+        generateBoard: generateBoard,
+        gameBoardArr: gameBoardArr
+    };
+
+})();
+gameBoardModule.generateBoard();
+ console.log(gameBoardModule.gameBoardArr);
+
+
+const displayController = (function() {
+     let clickCount = 0;
+    function clickCounter() {
+       
+        clickCount++
+        console.log(clickCount);
+    };
+
+    
+    function pickSq(pickedSq) {
+     let clickedSq =  document.getElementById(pickedSq);
+    //  let clickCount = 0;
+    //  clickCount++
+    if (clickedSq.classList.contains('empty') === true) {
+      if (clickCount === 0 || (clickCount % 2) === 0) {
+     console.log(clickedSq);
+     document.getElementById(pickedSq).classList.remove('empty');
+     clickedSq.classList.add('clickX');
+
+     } else  {
+         document.getElementById(pickedSq).classList.remove('empty')
+         document.getElementById(pickedSq).classList.add('clickO');
+    }   
+}
+    }
+    return {
+        
+        pickSq: pickSq,
+        clickCounter: clickCounter
+    }
+ 
+})();
+
+
+const playerFactory = function(name, xOrO) {
+    // let xOrO = 
+}
